94646c395f410a6b9acb3eca9a0ccf888d9f65f2
jankom <janko.itm@gmail.com>
2010-06-14 12:44:01 +0200

    nade native stack based, can reg natives now, polish whatever notation works now

--- toy.go
+++ toy.go
@@ -5,98 +5,142 @@ import fmt "fmt"
 import bufio "bufio"
 import os "os"
 import strconv "strconv"
+import list "container/list"
 
-var GLOB = make(map[string] Literal, 100);
-var NATIVE = make(map[string] Native, 100);
 
 const (
 	tnone	= iota
 	tstring = iota
 	tint	= iota
-	tfloat	= iota
 	tnative = iota
+	tword	= iota
 );
 
-type Literal struct {
+type ToyVal struct {
 	t int
-	sv string
-	iv int
-	fv float
+	s string
+	i int
 };
+
 
 type Native struct {
 	n	string
 	pc	int
-	f	func(a Literal, b Literal) Literal
+	f	func(s *list.List) ToyVal
 };
 
-func wordDefined(w string) bool {
-	return w == "add";
+var GLOB = make(map[string] ToyVal, 100);
+var NATIVE = make(map[string] Native, 100);
+var STACK = list.New();
+
+
+func getTag(tags []string, idx int) string {
+	var tag = strings.Trim(tags[idx], "\n\r");
+	//fmt.Printf("(tag: %s)", tag );
+	return tag;
 }
 
-func doTag(tags []string, idx int) Literal {
-	var tag = strings.Trim(tags[idx], "\n\r");
-	print("tag: " ); println(tag);
-	var iv, ier = strconv.Atoi(tag);
-	var fv, fer = strconv.Atof(tag);
-	if strings.HasSuffix(tag, ":") {
-		GLOB[tag] = doTag(tags, idx + 1);	
-	} else if strings.HasPrefix(tag, "\"") && strings.HasSuffix(tag, "\"") {
-		return Literal{t: tstring, sv: tag};
-	} else if ier == nil {
-		return Literal{t: tint, iv: iv};
-	} else if  fer == nil {
-		return Literal{t: tfloat, fv: fv};
-	} else if wordDefined(tag) {
-		//NATIVE[tag] != nil
-		if tag == "add"  {
-			return (NATIVE["add"].f)(doTag(tags, idx+1), doTag(tags, idx+2));
+func isSetWord(tag string) bool { 
+	return strings.HasSuffix(tag, ":"); 
+}
+
+func isString(tag string) bool { 
+	return strings.HasPrefix(tag, "\"") && strings.HasSuffix(tag, "\"");
+}
+
+func isInt(tag string) bool { 
+	var _, ier = strconv.Atoi(tag);
+	return ier == nil;
+}
+
+func isNative(tag string) bool { 
+	_, ok := NATIVE[tag];
+	return  ok;
+}
+
+func doTag(tags []string, idx int) (ToyVal, int) {
+	var tag = getTag(tags, idx);
+
+	if isSetWord(tag) {
+
+		GLOB[tag], idx = doTag(tags, idx + 1);	
+
+	} else if isString(tag) {
+
+		return ToyVal{t: tstring, s: tag}, idx + 1;
+
+	} else if isInt(tag) {
+
+		var iv, _ = strconv.Atoi(tag);
+		return ToyVal{t: tint, i: iv }, idx + 1;
+
+	} else if isNative(tag) {
+		var n = NATIVE[tag];
+		var s = list.New();
+		idx = idx + 1;
+		for i := 0; i<n.pc ; i++ {
+			var V ToyVal;
+			V, idx = doTag(tags, idx)
+			s.PushFront(V);
 		}
-		return doTag(tags, idx + 1);
+		return (NATIVE[tag].f)(s), idx;
+
 	} else {
-		println("Error: undefined")
+
+		fmt.Printf("\n***Error, undefined: %s\n", tag)
+
 	}
-	return Literal{t: tnone};
+	return ToyVal{t: tnone}, idx + 1;
 }
 
 func doTags(tags []string) {
-	for idx := range tags {
-		var l = doTag(tags, idx);
-		println(l.t); 
-		println(l.sv); 
-	}
+	doTag(tags, 0);
 }
 
 func printGLOB() {
 	for idx := range GLOB {
 		var v = GLOB[idx];
-		fmt.Printf("GLOB %s: %d, %s, %d, %f\n", idx, v.t, v.sv, v.iv, v.fv )
+		fmt.Printf("GLOB %s: %d, %s, %d\n", idx, v.t, v.s, v.i)
 	}
 }
 
-func regoEval (code string) {
-	println(code)
+func toyEval (code string) {
 	var tags = strings.Split(code, " ", 0);
 	doTags(tags);
 }
 
-func regNative (name string, pc int, f func(a Literal, b Literal) Literal) {
+func regNative (name string, pc int, f func(s *list.List)ToyVal) {
 	NATIVE[name] = Native{name, pc, f};
 }
 
 func natives() {
-	regNative("add", 2, func (a Literal, b Literal) Literal { return Literal{iv: a.iv + b.iv}; });
+	regNative("inc", 1, 
+		func (s *list.List) ToyVal { 
+			var v1 = s.Front().Value.(ToyVal);
+			return ToyVal{i: v1.i + 1 }; });
+	regNative("add", 2, 
+		func (s *list.List) ToyVal { 
+			var e1 = s.Front();
+			var v1 = e1.Value.(ToyVal);
+			var v2 ToyVal = e1.Next().Value.(ToyVal);
+			return ToyVal{i: v1.i + v2.i }; });
+	regNative("join", 2, 
+		func (s *list.List) ToyVal { 
+			var e1 = s.Front();
+			var v1 = e1.Value.(ToyVal);
+			var v2 ToyVal = e1.Next().Value.(ToyVal);
+			return ToyVal{s: v1.s + v2.s }; });
 }
 
 func main() {
-	println("REGO interpreter v0.0001");
+	println("~ Toylingo Stacked interpreter v0.0001 ~ ");
 	natives();
 	var in *bufio.Reader = bufio.NewReader(os.Stdin); 
 	var inp string;
-	for (inp != "exit\n") {
-		println(">>");
+	for (strings.Trim(inp, "\n\r") != "exit!\n") {
+		print(">>");
 		inp, _ := in.ReadBytes('\n')
-		regoEval(string(inp));
+		toyEval(string(inp));
 		printGLOB();
 	}
 }
